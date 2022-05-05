7e4bbfdf4b1ebbaf42687cc5775a4cce208a7aaa
Thomas Escrihuela <thomas.escrihuela@gmail.com>
2022-05-05 13:45:10 +0200

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Arbre généalogique en python
+
+Petit test vite fait de la librairie graphviz pour tester des graphs interactifs.
+
+Déploiement sur heroku : https://arbre-genealogique.herokuapp.com/
---
+++ app.py
@@ -0,0 +1,81 @@
+import dash_interactive_graphviz
+import dash
+import flask
+from dash import Input, Output, State, html
+from dash.dependencies import Input, Output
+import dash_html_components as html
+import dash_core_components as dcc
+import dash_bootstrap_components as dbc
+
+##################
+# Création du serveur
+server = flask.Flask(__name__)
+app = dash.Dash(__name__, server=server, title='Arbre généalogique')
+app.config.suppress_callback_exceptions = True
+
+
+initial_dot_source = """
+digraph  {
+node[style="filled"]
+"François 1er"->"Henri II"
+"Claude de France"->"Henri II"->"Charles IX"
+"Catherine de Médicis"->"Charles IX"
+}
+"""
+
+app.layout = html.Div(
+    [
+        html.Div(
+            dash_interactive_graphviz.DashInteractiveGraphviz(id="gv"),
+            style=dict(flexGrow=1, position="relative"),
+        ),
+        html.Div(
+            [
+                html.H3("Selected element"),
+                html.Div(id="selected"),
+                html.H3("Dot Source"),
+                dcc.Textarea(
+                    id="input",
+                    value=initial_dot_source,
+                    style=dict(flexGrow=1, position="relative"),
+                ),
+                html.H3("Engine"),
+                dcc.Dropdown(
+                    id="engine",
+                    value="dot",
+                    options=[
+                        dict(label=engine, value=engine)
+                        for engine in [
+                            "dot",
+                            "fdp",
+                            "neato",
+                            "circo",
+                            "osage",
+                            "patchwork",
+                            "twopi",
+                        ]
+                    ],
+                ),
+            ],
+            style=dict(display="flex", flexDirection="column"),
+        ),
+    ],
+    style=dict(position="absolute", height="100%", width="100%", display="flex"),
+)
+
+
+@app.callback(
+    [Output("gv", "dot_source"), Output("gv", "engine")],
+    [Input("input", "value"), Input("engine", "value")],
+)
+def display_output(value, engine):
+    return value, engine
+
+
+@app.callback(Output("selected", "children"), [Input("gv", "selected")])
+def show_selected(value):
+    return html.Div(value)
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
